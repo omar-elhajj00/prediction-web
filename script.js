22cf62b5857fea83ff9f574711b1bb77392ceb8c
Omar El Hajj <omar.elhajj.00@gmail.com>
2022-09-10 12:43:18 +0300

    commented the setDogImage function in javascript file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Define the variable based on their ids
 let submitButton = document.getElementById('submitbtn')
 let gender = document.querySelector('#gender');
 let nationality = document.querySelector('#nationality');
@@ -7,8 +8,10 @@ const myImg = document.getElementById('dogimg');
 
 submitButton.addEventListener("click", getAllInfo);
 
+//call the function that request the image apiapi
 setDogImage();
 
+//Dog's image api
 async function setDogImage() {
     let getDogImage = await fetch('https://dog.ceo/api/breeds/image/random');
     let dogImg = await getDogImage.json();
@@ -16,18 +19,21 @@ async function setDogImage() {
     myImg.src = dogImg.message;
 }
 
+//function to be called when submit button clicked 
 async function getAllInfo() {
     
-    const userName = document.querySelector('#username').value;
-    let getGender = await fetch(`https://api.genderize.io?name=${userName}`);
-    let genderData = await getGender.json();
-    gender.innerHTML = genderData.gender;
+    const userName = document.querySelector('#username').value; //get the username value
 
-    let getNationality = await fetch(`https://api.nationalize.io/?name=${userName}`);
-    let nationalityData = await getNationality.json();
+    let getGender = await fetch(`https://api.genderize.io?name=${userName}`); //send the gender api  
+    let genderData = await getGender.json(); //receive the response and implement it in  json object
+    gender.innerHTML = genderData.gender; // make the inner text of gender as gender json object
+    
+    //nationality api
+    let getNationality = await fetch(`https://api.nationalize.io/?name=${userName}`); //send the nationality api  
+    let nationalityData = await getNationality.json();//receive the response and implement it in json object
     nationality.innerHTML = `${ nationalityData.country[0].country_id } or ${ nationalityData.country[1].country_id }`;
-    console.log(nationalityData.country[0].country_id);
 
+    //age api
     let getAge = await fetch(`https://api.agify.io/?name=${userName}`);
     let ageData = await getAge.json();
     age.innerHTML = ageData.age;
