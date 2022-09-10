bb07e50993b1d925165200b43964830033a15062
Omar El Hajj <omar.elhajj.00@gmail.com>
2022-09-10 18:18:55 +0300

    commented the html and css code

--- script.js
+++ script.js
@@ -21,55 +21,24 @@ async function setDogImage() {
 
 //function to be called when submit button clicked 
 async function getAllInfo() {
+    //get the username value
+    const userName = document.querySelector('#username').value; 
     
-    const userName = document.querySelector('#username').value; //get the username value
-
+    // gender api request
     let getGender = await fetch(`https://api.genderize.io?name=${userName}`); //send the gender api  
     let genderData = await getGender.json(); //receive the response and implement it in  json object
     gender.innerHTML = genderData.gender; // make the inner text of gender as gender json object
     
-    //nationality api
+    //nationality api request 
     let getNationality = await fetch(`https://api.nationalize.io/?name=${userName}`); //send the nationality api  
     let nationalityData = await getNationality.json();//receive the response and implement it in json object
-    nationality.innerHTML = `${ nationalityData.country[0].country_id } or ${ nationalityData.country[1].country_id }`;
+    nationality.innerHTML = `${ nationalityData.country[0].country_id } or ${ nationalityData.country[1].country_id }`; //make the first 2 element of nationalities array inside the country tag
 
-    //age api
+    //age api request
     let getAge = await fetch(`https://api.agify.io/?name=${userName}`);
     let ageData = await getAge.json();
     age.innerHTML = ageData.age;
-
-    // let getNationality = await fetch(' https://api.nationalize.io/?name=mohamad');
-
-    // let myDogImage = await getDogImage.formData();
-    // console.log(getGender.status); // 400
-    // console.log(getAge.text());
-    // api.innerHTML = getAge.text();
-    // console.log(getNationality.statusText); 
-    // let dogImage = document.querySelector('#dogimg')
-    // dogImage.src = myDogImage;
-    
-
-    // api.innerHTML = await getGender.text();
 }
 
-// const res = await fetch(genderURL);
-//         const data = await res.json();
-//         return data.gender;
-// async function haha() {
-//     let getGender = await fetch('https://api.genderize.io?name=rio');
-//     let getAge = await fetch(' https://api.agify.io/?name='+userName);
-//     let getNationality = await fetch(' https://api.nationalize.io/?name=mohamad');
-//     let getDogImage = await fetch('https://dog.ceo/api/breeds/image/random');
-//     // let myDogImage = await getDogImage.formData();
-//     console.log(getGender.status); // 400
-//     console.log(getAge.text());
-//     console.log(getNationality.statusText); 
-//     // let dogImage = document.querySelector('#dogimg')
-//     // dogImage.src = myDogImage;
-    
-//     let api = document.querySelector('#api');
-//     let api1 = document.querySelector('#api1');
-//     api.innerHTML = await getGender.text();
-// }
 
 
